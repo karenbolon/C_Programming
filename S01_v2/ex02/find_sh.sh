7c137184ec521882c548a60c8f8b6a38cab9792d
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2023-04-04 13:01:19 +0200

    Delete S01_v2 directory

--- S01_v2/ex02/find_sh.sh
+++
@@ -1,3 +0,0 @@
-# the 's' is substitute in sed, then regex (look for files with *.sh)/replacement (remove with sed)/flags */
-#find . -type f -name "*\.sh" | sed "s/[.]sh*$//"
-find . -type f -name "*.sh" -exec basename {} .sh \;
