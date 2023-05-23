b288a9f97aac68a90dcd2ec8ba39614f9a6b699e
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2023-05-23 09:42:48 +0200

    fixed strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,49 +6,51 @@
 /*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 14:21:59 by kbolon            #+#    #+#             */
-/*   Updated: 2023/05/20 10:18:44 by kbolon           ###   ########.fr       */
+/*   Updated: 2023/05/23 09:25:58 by kbolon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*trim_string(const char *s1, size_t start, size_t end)
+static int	ft_check_set(char const c, char const *set)
 {
-	size_t	i;
-	size_t	len;
-	char	*trimmed;
+	int	i;
 
 	i = 0;
-	if (end >= start)
-		len = end - start + 1;
-	else
-		len = 0;
-	trimmed = (char *)malloc(sizeof(char) * (len + 1));
-	if (trimmed == 0)
-		return (0);
-	trimmed[len] = '\0';
-	while (i < len)
+	while (set[i] != '\0')
 	{
-		trimmed[i] = s1[start++];
+		if (set[i] == c)
+			return (1);
 		i++;
 	}
-	return (trimmed);
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	start;
-	size_t	end;
-	char	*trimmed;
+	size_t	size;
+	char	*new;
 
-	start = 0;
-	if (s1 == 0 || set == 0)
-		return (0);
-	while (s1[start] && strchr(set, s1[start]))
-		start++;
-	end = ft_strlen(s1) - 1;
-	while (end > start && strchr(set, s1[end]))
-		end--;
-	trimmed = trim_string(s1, start, end);
-	return (trimmed);
+	if (!s1 || !set)
+		return (NULL);
+	while (s1)
+	{
+		if (ft_check_set(((char)*s1), set) == 1)
+			s1++;
+		else
+			break ;
+	}
+	size = ft_strlen(s1);
+	while (size != 0)
+	{
+		if (ft_check_set(s1[size - 1], set) == 1)
+			size--;
+		else
+			break ;
+	}
+	new = (char *)malloc(size * sizeof(char) + 1);
+	if (!new)
+		return (NULL);
+	ft_strlcpy(new, (char *)s1, size + 1);
+	return (new);
 }
